a2cb9dc11d7290c8bbc69ea1b48987a8141acaef
Pavitra-coder <pavitrabhalke456@gmail.com>
2020-04-30 22:56:41 +0530

    Refactoring code

---
+++ EmpWageBuilderClassMethod7.java
@@ -0,0 +1,9 @@
+public  class EmpWageBuilderClassMethod7
+{
+
+        public static void main(String[] args)
+        {
+                EmpUtil7 emp=new EmpUtil7();
+                emp.computeEmpWage();
+        }
+}
